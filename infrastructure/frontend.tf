ba97702fc74bf0ad58a5d1dbc72bbc6e28f1d72d
shaafekhan <56558384+shaafekhan@users.noreply.github.com>
2023-10-15 23:19:26 +0200

    add policy

--- infrastructure/frontend.tf
+++ infrastructure/frontend.tf
@@ -31,7 +31,7 @@ resource "aws_s3_bucket_public_access_block" "crc-frontend-public-access" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "crc-frontend-acl" {
   depends_on = [
     aws_s3_bucket_ownership_controls.crc-frontend-ownership-control,
     aws_s3_bucket_public_access_block.crc-frontend-public-access,
@@ -39,6 +39,30 @@ resource "aws_s3_bucket_acl" "example" {
 
   bucket = aws_s3_bucket.crc-frontend.id
   acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "public-access" {
+  bucket = aws_s3_bucket.crc-frontend.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "public-access" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.crc-frontend.arn,
+      "${aws_s3_bucket.crc-frontend.arn}/*",
+    ]
+  }
 }
 
 
