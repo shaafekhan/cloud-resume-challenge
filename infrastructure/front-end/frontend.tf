e783ce8b419cd1b23ef28f99ab5cffb4e2bd0e9b
shaafekhan <56558384+shaafekhan@users.noreply.github.com>
2023-10-15 20:37:24 +0200

    add frontend tf

---
+++ infrastructure/front-end/frontend.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.21.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
