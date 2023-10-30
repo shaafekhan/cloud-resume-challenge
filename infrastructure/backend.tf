6cea0ba65da6775c6e02d1e6cd747a89a47c2074
shaafekhan <56558384+shaafekhan@users.noreply.github.com>
2023-10-30 19:40:37 +0100

    create item

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -9,3 +9,13 @@ resource "aws_dynamodb_table" "crc-backend-database" {
     }
   
 }
+resource "aws_dynamodb_table_item" "crc-backend-database-item" {
+  table_name = aws_dynamodb_table.crc-backend-database.name
+  hash_key   = aws_dynamodb_table.crc-backend-database.hash_key
+
+  item = <<ITEM
+{
+  "id": {"N": "0"}
+}
+ITEM
+}
